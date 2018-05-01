f395f58ad8bf544649e9011c0d495631cf8e4c59
Stephen Engler <sengler1989@gmail.com>
2018-05-01 13:58:58 -0500

    deck post route

--- server/routes/deck.router.js
+++ server/routes/deck.router.js
@@ -22,12 +22,44 @@ router.get('/', (req, res) => {
         res.sendStatus(403);
     }
 });
+//make sure to as about this
+router.get('/cards/:id', (req, res)=>{
+    console.log('in router get cards')
+    if(req.isAuthenticated()){
+        const queryText = `SELECT * FROM "flashcard"."card" WHERE deck_id = $1;`;
+        pool.query(queryText, [req.params.id])
+            .then((result)=>{
+                console.log(result.rows);
+                res.send(result.rows)
+            })
+            .catch((error)=>{
+                console.log('error in get cards route, ', error);
+            })
+    }
+    else{
+        res.sendStatus(403);
+    }
+})
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
+    if(req.isAuthenticated()){
+        const queryText = `INSERT INTO "flashcard"."deck" 
+                            ("user_id", "deck_name") 
+                            VALUES ($1, $2) RETURNING *;`;
 
+        pool.query(queryText, [req.user.id, req.body.deck_name])
+            .then((result)=>{
+                res.send(result.rows)
+            })
+            .catch((error)=>{
+                console.log('error from server in post deck route', error)
+            })
+    }else{
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
